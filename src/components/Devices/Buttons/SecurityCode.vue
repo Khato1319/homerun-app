041c84e83798e04507a9709d86d92c05d194b4e6
AleCaeiro <acaeiro@itba.edu.ar>
2022-05-09 17:55:56 -0300

    Avance en componentes para los dispositivos

---
+++ src/components/Devices/Buttons/SecurityCode.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-form>
+    <v-container>
+      <v-row>
+        <v-col cols="2">
+          <v-text-field
+              prepend-icon="mdi-lock"
+              :append-icon="hide ? 'mdi-eye-off' : 'mdi-eye'"
+              outlined
+              :type="hide ? 'password' : 'text'"
+              @click:append="hide = !hide"
+
+
+          >
+          </v-text-field>
+
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+export default {
+
+  props: ['password'],
+  data (){
+    return {
+      hide: true,
+      correctPassword: '1111',
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
