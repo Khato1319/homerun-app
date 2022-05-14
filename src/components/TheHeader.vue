4537728e6608b7daec836ec5727f20b65396d6d3
AleCaeiro <acaeiro@itba.edu.ar>
2022-05-14 16:12:51 -0300

    Completo menu de ayuda

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -3,14 +3,28 @@
       elevation="4"
       color="primary"
   >
-    <v-btn text width="40" height="50" @click="$emit('onClick')" class="mr-2">
-      <v-img height="50" width="50" src="@/assets/HR-Logo-Nameless-Shape.png"/>
-    </v-btn>
-<!--    Podría ponerse un espacio porque por ahora queda muy pegado-->
+    <v-tooltip bottom>
+      <template v-slot:activator="{on, attrs}">
+        <!--    Podría ponerse un espacio porque por ahora queda muy pegado-->
+        <v-btn text width="40" height="50" @click="$emit('onClick')" class="mr-2" v-bind="attrs" v-on="on">
+          <v-img height="50" width="50" src="@/assets/HR-Logo-Nameless-Shape.png"/>
+        </v-btn>
+      </template>
+      <span>Página principal de HomeRun</span>
+    </v-tooltip>
+
+
     <v-toolbar-title class="white--text mr-auto" style="font-family:Shrikhand,Nunito">HomeRun</v-toolbar-title>
-    <v-btn color="blue lighten-4" rounded @click="()=>$emit('help-event')">
-      <v-icon>mdi-help</v-icon>
-    </v-btn>
+
+    <v-tooltip bottom>
+      <template v-slot:activator="{on, attrs}">
+        <v-btn color="white" class="mr-2" fab small outlined @click=openHelp v-bind="attrs" v-on="on" >
+          <v-icon>mdi-help</v-icon>
+        </v-btn>
+      </template>
+      <span>Ayuda</span>
+    </v-tooltip>
+
   </v-app-bar>
 </template>
 
--- src/components/ViewButtons/HelpView.vue
+++ src/components/ViewButtons/HelpView.vue
@@ -10,29 +10,187 @@
     </v-row>
     <v-row justify="center">
       <v-col>
+        <div class="primary--text text-sm-start mb-2">
+          Agregar habitaciones, rutinas y dispositivos
+        </div>
+
         <v-expansion-panels inset>
+
           <v-expansion-panel>
-            <v-expansion-panel-header>Como agregar una habitacion</v-expansion-panel-header>
+            <v-expansion-panel-header>
+              ¿Cómo agrego una habitación?
+              <template v-slot:actions>
+                <v-icon color="primary">$expand</v-icon>
+              </template>
+            </v-expansion-panel-header>
             <v-expansion-panel-content>
               <ol class="ordered-list">
-                <li>Ir al menu "Habitaciones"</li>
-                <li>Hacer click en el boton "+" que se encuentra debajo a la derecha</li>
-                <li>Elegir el nombre que desee para su nueva habitacion</li>
+                <li>En la parte superior, toca <b>Habitaciones</b></li>
+                <li>Desplázate hasta la parte inferior y luego toca <v-icon color="primary">mdi-plus-circle</v-icon></li>
+                <li>Cuando termines de colocar el nombre, presiona <b>enter</b> en tu teclado</li>
               </ol>
             </v-expansion-panel-content>
           </v-expansion-panel>
 
           <v-expansion-panel>
-            <v-expansion-panel-header >Como editar una habitacion </v-expansion-panel-header>
+            <v-expansion-panel-header>
+              ¿Cómo agrego una rutina?
+              <template v-slot:actions>
+                <v-icon color="primary">$expand</v-icon>
+              </template>
+            </v-expansion-panel-header>
             <v-expansion-panel-content>
               <ol class="ordered-list">
-                <li>Ir al menu "Habitaciones"</li>
-                <li>Hacer click en el boton "editar" que se encuentra arriba a la izquierda</li>
-                <li>Utilice el lapiz para cambiar el nombre de la habitacion o el tacho de basura para eliminar la misma</li>
+                <li>En la parte superior, toca <b>Rutinas</b></li>
+                <li>Desplázate hasta la parte inferior y luego toca <v-icon color="primary">mdi-plus-circle</v-icon></li>
+                <li>Cuando termines de colocar el nombre, presiona <b>Enter</b> en tu teclado</li>
+                <li>Desplázate a la solapa derecha y, luego seleccione un dispositivo que quiera añadir</li>
+                <li>Selecciona la acción que quieras que realice el dispositivo</li>
+                <li>Cuando termines, toca <b>Agregar</b></li>
+              </ol>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+
+          <v-expansion-panel>
+            <v-expansion-panel-header>
+              ¿Cómo agrego un dispositivo?
+              <template v-slot:actions>
+                <v-icon color="primary">$expand</v-icon>
+              </template>
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <ol class="ordered-list">
+                <li>En la parte superior, toca <b>Habitaciones</b></li>
+                <li>Toca la habitacion donde quieras agregar el dispositivo</li>
+                <li>Desplázate a la solapa derecha, y luego toca <v-icon color="primary">mdi-plus-circle</v-icon></li>
+                <li>Completa la información de tu dispositivo (nombre, tipo, grupo y autenticación)</li>
+                <li>Cuando termines toca <b>Agregar</b></li>
               </ol>
             </v-expansion-panel-content>
           </v-expansion-panel>
         </v-expansion-panels>
+
+        <div class="primary--text text-sm-start mb-2 mt-8">
+          Editar habitaciones, rutinas y dispoitivos
+        </div>
+
+        <v-expansion-panels inset>
+          <v-expansion-panel>
+            <v-expansion-panel-header>
+              ¿Cómo edito una habitación?
+              <template v-slot:actions>
+                <v-icon color="primary">$expand</v-icon>
+              </template>
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <ol class="ordered-list">
+                <li>En la parte superior, toca <b>Habitaciones</b></li>
+                <li>Desplázate hasta la parte superior y, luego toca <v-icon color="primary">mdi-pencil-box</v-icon></li>
+                <li>Toca <v-icon>mdi-pencil-circle</v-icon> en la habitacion que quieras editar</li>
+                <li>Cuando termines de modificar el nombre, toca <b>enter</b> en tu teclado</li>
+              </ol>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+
+          <v-expansion-panel>
+            <v-expansion-panel-header>
+              ¿Cómo edito una rutina?
+              <template v-slot:actions>
+                <v-icon color="primary">$expand</v-icon>
+              </template>
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <ol class="ordered-list">
+                <li>En la parte superior, toca <b>Rutinas</b></li>
+                <li>Desplázate hasta la parte superior, y luego toca <v-icon color="primary">mdi-pencil-box</v-icon></li>
+                <li>Toca <v-icon>mdi-pencil-circle</v-icon> en la rutina que quieras editar</li>
+                <li>Cuando termines de modificar el nombre, toca <b>enter</b> en tu teclado</li>
+
+              </ol>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+
+
+          <v-expansion-panel>
+            <v-expansion-panel-header>
+              ¿Como edito un dispositivo?
+              <template v-slot:actions>
+                <v-icon color="primary">$expand</v-icon>
+              </template>
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <ol class="ordered-list">
+                <li>En la parte superior, toca <b>Habitaciones</b></li>
+                <li>Toca la habitacion donde quieras editar el dispositivo</li>
+                <li>Desplázate a la solapa derecha, y luego toca <v-icon color="primary">mdi-pencil-box</v-icon></li>
+                <li>Toca <v-icon>mdi-pencil-circle</v-icon> en el dispositivo que quieras editar</li>
+                <li>Cuando termines de modificar el nombre, toca <b>enter</b> en tu teclado</li>
+
+              </ol>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+
+        <div class="primary--text text-sm-start mb-2 mt-8">
+          Eliminar habitaciones, rutinas y dispoitivos
+        </div>
+
+        <v-expansion-panels inset>
+          <v-expansion-panel>
+            <v-expansion-panel-header>
+              ¿Cómo elimino una habitación?
+              <template v-slot:actions>
+                <v-icon color="primary">$expand</v-icon>
+              </template>
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <ol class="ordered-list">
+                <li>En la parte superior, toca <b>Habitaciones</b></li>
+                <li>Desplázate hasta la parte superior y, luego toca <v-icon color="primary">mdi-pencil-box</v-icon></li>
+                <li>Toca <v-icon>mdi-delete-circle</v-icon> en la habitacion que quieras eliminar</li>
+                <li>En la confirmacion de la accion, toca <b>Borrar</b></li>
+              </ol>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+
+          <v-expansion-panel>
+            <v-expansion-panel-header>
+              ¿Cómo elimino una rutina?
+              <template v-slot:actions>
+                <v-icon color="primary">$expand</v-icon>
+              </template>
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <ol class="ordered-list">
+                <li>En la parte superior, toca <b>Rutinas</b></li>
+                <li>Desplázate hasta la parte superior, y luego toca <v-icon color="primary">mdi-pencil-box</v-icon></li>
+                <li>Toca <v-icon>mdi-delete-circle</v-icon> en la rutina que quieras eliminar</li>
+                <li>Para confirmar la acción, toca <b>Borrar</b></li>
+              </ol>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+
+
+          <v-expansion-panel>
+            <v-expansion-panel-header>
+              ¿Cómo elimino un dispositivo?
+              <template v-slot:actions>
+                <v-icon color="primary">$expand</v-icon>
+              </template>
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <ol class="ordered-list">
+                <li>En la parte superior, toca <b>Habitaciones</b></li>
+                <li>Toca la habitación donde quieras eliminar el dispositivo</li>
+                <li>Desplázate a la solapa derecha, y luego toca <v-icon color="primary">mdi-pencil-box</v-icon></li>
+                <li>Toca <v-icon>mdi-delete-circle</v-icon> en el dispositivo que quieras editar</li>
+                <li>Para confirmar la acción, toca <b>Borrar</b></li>
+              </ol>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+
+
       </v-col>
     </v-row>
   </v-container>
